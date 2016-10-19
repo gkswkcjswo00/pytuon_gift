1eec85f205a5c0b113d9133e0d5b24db5e851a91
gkswkcjswo00 <gkswkcjswo00@naver.com>
2016-10-19 20:04:32 +0900

    Add files via upload

---
+++ 로또.py
@@ -0,0 +1,22 @@
+import random
+
+
+while True:
+    a =(random.sample(range(1,46),6))
+
+    b =(random.sample(range(1,46),1))
+
+    if a != b:
+       break;
+
+print(a)
+
+print ("BONUS",(b))
+    
+
+       
+
+
+    
+
+           
