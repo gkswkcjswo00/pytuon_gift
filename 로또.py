39926d8171eb21632de95d6303061c612381ed69
gkswkcjswo00 <gkswkcjswo00@naver.com>
2016-10-29 08:52:38 +0900

    Update 로또.py

--- 로또.py
+++ 로또.py
@@ -1,22 +1,11 @@
 import random
 
 
-while True:
-    a =(random.sample(range(1,46),6))
 
-    b =(random.sample(range(1,46),1))
-
-    if a != b:
-       break;
-
-print(a)
-
-print ("BONUS",(b))
-    
-
-       
+a =(random.sample(range(1,46),7))
 
 
-    
 
-           
+   
+print(a[0],a[1],a[2],a[3],a[4],a[5])
+print("BONUS",a[6])
